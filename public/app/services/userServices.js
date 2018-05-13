f9e3a6de98c485405f7af83dd378f224e6a1634b
SInichiKudo <ditohafizh.gmail.com>
2018-05-13 17:40:30 +0700

    Updated back end for regist and login

--- public/app/services/userServices.js
+++ public/app/services/userServices.js
@@ -1,15 +1,19 @@
 // SERVICES FOR USER LOGIN
 angular.module('userServices', [])
-    .factory('User', function($http){
+    .factory('User', function ($http) {
         userFactory = {};
 
         //debug
-        userFactory.debug = function(debugData){
-          return $http.post('/api/imgUpload', debugData);
+
+        userFactory.create = function (regData) {
+            return $http.post('/userApi/users', regData);
         };
 
-        userFactory.create = function(regData){
-            return $http.post('/api/users', regData);
+        userFactory.uploadImage = function (image) {
+            return $http.post('/userApi/uploadImage', image, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            });
         };
 
         return userFactory;
